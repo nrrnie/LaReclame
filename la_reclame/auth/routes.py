9af758667102233cdcbff4d338bacabef7f6065e
mercurial <dias.nespayev@gmail.com>
2022-05-10 15:18:48 +0600

    chore: add login page

--- la_reclame/auth/routes.py
+++ la_reclame/auth/routes.py
@@ -1,1 +1,8 @@
+from flask import request, render_template
 from la_reclame.auth import auth
+
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
