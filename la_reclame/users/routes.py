29e2b2b95e9a2f26e2db19e88e7069178bb24cac
mercurial <dias.nespayev@gmail.com>
2022-05-10 17:57:37 +0600

    feat: add profile page

--- la_reclame/users/routes.py
+++ la_reclame/users/routes.py
@@ -1,2 +1,7 @@
+from flask import render_template, session
 from la_reclame.users import users
 
+
+@users.route('/profile')
+def profile():
+    return render_template('profile.html', user=session['user'])
