fb2b267c4972a99ebc125ccb2c99cc9a77ea0452
mercurial <dias.nespayev@gmail.com>
2022-05-10 14:09:54 +0600

    chore: add models

---
+++ la_reclame/models.py
@@ -0,0 +1,19 @@
+from la_reclame import db
+from datetime import datetime
+
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), nullable=False, unique=True)
+    password = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    registered = db.Column(db.DATETIME, nullable=False, default=datetime.now)
+
+
+class Items(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    author_username = db.Column(db.String(255), nullable=False)
+    created = db.Column(db.DATETIME, nullable=False, default=datetime.now)
+    title = db.Column(db.String(255), nullable=False)
+    body = db.Column(db.String(255), nullable=False)
+    is_active = db.Column(db.Boolean, nullable=False, default=True)
