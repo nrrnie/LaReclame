a4f25dfe05309341f1853696670f3384a6c6f974
mercurial <dias.nespayev@gmail.com>
2022-05-10 17:46:03 +0600

    chore: add items page

--- la_reclame/items/routes.py
+++ la_reclame/items/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, session
 from la_reclame.items import items
 from utils import auth_required
 
@@ -6,4 +6,4 @@ from utils import auth_required
 @items.route('/')
 @auth_required
 def items_home():
-    return render_template('items.html')
+    return render_template('items.html', user=session['user'])
