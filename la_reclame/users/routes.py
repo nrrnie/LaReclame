79f17c51cee190089de7f91e83aba1016a08ca9b
mercurial <dias.nespayev@gmail.com>
2022-05-10 22:51:50 +0600

    feat: update info about user

--- la_reclame/users/routes.py
+++ la_reclame/users/routes.py
@@ -1,7 +1,9 @@
-from flask import render_template
+from flask import render_template, request, session
+from flask import flash
 from la_reclame.users import users
 from la_reclame.models import Users
 from utils import auth_required
+from la_reclame import db
 
 
 @users.route('/<username>')
@@ -13,3 +15,24 @@ def profile(username: str):
         return '404\nUser not found'
 
     return render_template('profile.html', user=user)
+
+
+@users.route('/settings', methods=['GET', 'POST'])
+@auth_required
+def settings():
+    if request.method == 'GET':
+        return render_template('settings.html', user=session['user'])
+
+    username = request.form.get('username')
+
+    if Users.query.filter_by(username=username).first() is not None:
+        flash('Username is already taken', 'danger')
+    else:
+        user = Users.query.get(session['user'].id)
+        user.username = username
+        db.session.commit()
+
+        session['user'] = user
+
+        flash('Info was updated', 'success')
+    return render_template('settings.html', user=session['user'])
