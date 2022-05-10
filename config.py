80d6235ec47145683ffafa166b5dd0d094a38758
mercurial <dias.nespayev@gmail.com>
2022-05-10 14:02:48 +0600

    chore: add app config

---
+++ config.py
@@ -0,0 +1,20 @@
+from dotenv import load_dotenv
+from os import environ
+
+from urllib.parse import quote_plus
+
+load_dotenv()
+
+db = {
+    'user': environ.get('DATABASE_USER'),
+    'password': environ.get('DATABASE_PASSWORD'),
+    'host': environ.get('DATABASE_HOST'),
+    'name': environ.get('DATABASE_NAME')
+}
+
+
+class Config:
+    SECRET_KEY = 'asd'
+    SESSION_TYPE = 'sqlalchemy'
+    SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{db["user"]}:{quote_plus(db["password"])}@{db["host"]}/{db["name"]}'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
