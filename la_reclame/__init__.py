1b4c347b38154349af88129d02c4b76647904886
mercurial <dias.nespayev@gmail.com>
2022-05-10 14:10:06 +0600

    chore: add SQLAlchemy

--- la_reclame/__init__.py
+++ la_reclame/__init__.py
@@ -1,9 +1,16 @@
+from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 from config import Config
+
+
+db = SQLAlchemy()
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+
+    with app.app_context():
+        db.init_app(app)
 
     return app
