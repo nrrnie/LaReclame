f0c732289230d9d4189ee9c5ea64d5bf4132cd50
mercurial <dias.nespayev@gmail.com>
2022-05-11 03:20:58 +0600

    chore: show items

--- la_reclame/items/routes.py
+++ la_reclame/items/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, session, request, flash
 from la_reclame.items import items
-from la_reclame.models import Items
+from la_reclame.models import Items, Users
 from la_reclame import db
 from utils import auth_required
 
@@ -8,7 +8,7 @@ from utils import auth_required
 @items.route('/')
 @auth_required
 def items_home():
-    return render_template('items.html', user=session['user'])
+    return render_template('items.html', user=session['user'], items=Items.query.all())
 
 
 @items.route('/add-item', methods=['GET', 'POST'])
