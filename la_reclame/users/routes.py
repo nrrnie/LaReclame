289be75663efdc0950fbcd2d7f79ace577aba8a2
mercurial <dias.nespayev@gmail.com>
2022-05-10 18:01:02 +0600

    feat: return profile page

--- la_reclame/users/routes.py
+++ la_reclame/users/routes.py
@@ -1,7 +1,13 @@
 from flask import render_template, session
 from la_reclame.users import users
+from la_reclame.models import Users
 
 
-@users.route('/profile')
-def profile():
-    return render_template('profile.html', user=session['user'])
+@users.route('/<username>')
+def profile(username: str):
+    user = Users.query.filter_by(username=username).first()
+
+    if user is None:
+        return '404\nUser not found'
+
+    return render_template('profile.html', user=user)
