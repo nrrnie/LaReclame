8602a9b32c3ff3255f541d46a718fe5904868a8b
mercurial <dias.nespayev@gmail.com>
2022-05-11 00:19:15 +0600

    feat: add item from user

--- la_reclame/items/routes.py
+++ la_reclame/items/routes.py
@@ -1,5 +1,7 @@
-from flask import render_template, session
+from flask import render_template, session, request, flash
 from la_reclame.items import items
+from la_reclame.models import Items
+from la_reclame import db
 from utils import auth_required
 
 
@@ -7,3 +9,23 @@ from utils import auth_required
 @auth_required
 def items_home():
     return render_template('items.html', user=session['user'])
+
+
+@items.route('/add-item', methods=['GET', 'POST'])
+@auth_required
+def add_item():
+    if request.method == 'GET':
+        return render_template('add-item.html', user=session['user'])
+
+    title = request.form.get('item-title')
+    description = request.form.get('item-description')
+
+    item = Items(user_id=session['user'].id, title=title, description=description)
+    db.session.add(item)
+    db.session.commit()
+
+    flash('Item is added!', 'success')
+    return render_template('add-item.html', user=session['user'])
+
+
+
