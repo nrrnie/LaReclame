4621747009330f073ba2ac590eda59438103bcba
mercurial <dias.nespayev@gmail.com>
2022-05-11 17:12:51 +0600

    feat: pass table name

--- la_reclame/utils/routes.py
+++ la_reclame/utils/routes.py
@@ -3,8 +3,8 @@ from la_reclame.utils import utils
 from utils import picturesDB
 
 
-@utils.route('/get-picture/<filename>', methods=['GET'])
-def get_picture(filename: str):
-    path = picturesDB.get_picture_path('profile-pictures', filename)
+@utils.route('/get-picture/<table>/<filename>', methods=['GET'])
+def get_picture(table: str, filename: str):
+    path = picturesDB.get_picture_path(table, filename)
     return send_file(path, mimetype='image/gif')
 
