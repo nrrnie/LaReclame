b974bc5623c312e97d761bf8980cce173d9ec23f
mercurial <dias.nespayev@gmail.com>
2022-05-11 23:53:41 +0600

    fix: path to pictures

--- la_reclame/utils/routes.py
+++ la_reclame/utils/routes.py
@@ -1,10 +1,13 @@
-from flask import send_file
+from flask import request, send_file
 from la_reclame.utils import utils
 from utils import picturesDB
 
 
-@utils.route('/get-picture/<table>/<filename>', methods=['GET'])
-def get_picture(table: str, filename: str):
+@utils.route('/get-picture', methods=['GET'])
+def get_picture():
+    table = request.values.get('table')
+    filename = request.values.get('filename')
+
     path = picturesDB.get_picture_path(table, filename)
     return send_file(path, mimetype='image/gif')
 
