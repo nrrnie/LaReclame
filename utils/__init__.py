cf0d29840662aa6f191a34133265e65f3da955d2
mercurial <dias.nespayev@gmail.com>
2022-05-18 16:12:19 +0600

    feat: add `send_email` function

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,9 @@
 from flask import session, redirect, url_for
-import functools
 from utils.PicturesDB import PicturesDB
+from email.message import EmailMessage
+from smtplib import SMTP_SSL
+from os import getenv
+import functools
 picturesDB = PicturesDB()
 
 
@@ -22,3 +25,15 @@ def not_auth(func):
         return redirect(url_for('items.items_home'))
 
     return wrapper
+
+
+def send_email(to: str, token_link: str):
+    message = EmailMessage()
+    message['Subject'] = 'Email verification'
+    message['From'] = getenv('SMTP_SENDER')
+    message['To'] = to
+    message.set_content('Your verification link is %s' % token_link)
+
+    with SMTP_SSL('smtp.gmail.com', int(getenv('SMTP_PORT'))) as smtp:
+        smtp.login(getenv('SMTP_SENDER'), getenv('SMTP_PASSWORD'))
+        smtp.send_message(message)
