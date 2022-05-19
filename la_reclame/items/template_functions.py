5efa00b042dc5c8b5205f11a8e6812829d678c7d
mercurial <dias.nespayev@gmail.com>
2022-05-19 11:10:59 +0600

    fix: big descriptions

--- la_reclame/items/template_functions.py
+++ la_reclame/items/template_functions.py
@@ -5,3 +5,8 @@ from la_reclame.models import Users
 @items.app_template_global()
 def get_user_by_id(user_id: int):
     return Users.query.get(user_id)
+
+
+@items.app_template_global()
+def cut_description(description: str, chars: int):
+    return description[:chars] + '...' if len(description) > chars else description[:chars]
